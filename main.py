9c33db2bbae93ff3d1f4256ba7c81a45c57415aa
Nicolas <nicolasguedesguedes081@gmail.com>
2025-06-12 03:52:59 -0300

    atualização

--- main.py
+++ main.py
@@ -5,73 +5,39 @@ while True:
     try:
         opcao=int(input("""
 1- CADASTRO
-2- LOGIN
+2- BUSCAR USUARIO
 3- SAIR
 digite a sua opção aqui:"""))
-        
-        
-        match opcao:
-            case 1:
-                
-                while True:
-                    user_name=input("digite seu nome:")
-                    senha_escolha=input("deseja ter uma senha aleatoria(s ou n)?").lower()
-                    if senha_escolha == 's':
-                        user_senha=cliente.senha_aleatoria
-                        print("1- ver senha")
-                        print("2- nao ver senha")
-                        try:
-                            senha_ver=int(input("escolha uma das opções:"))
-                        except:
-                            print("formato invalido")
-                        if senha_ver != 1 or senha_ver != 2:
-                            print("invalido")
-                        else:
-                            print(user_senha)
-                    else:
-                        while True:
-                            
-                            user_senha=input("digite sua senha:")
-                            cliente.validar_senha(user_senha)
-                            if cliente.validar_senha(user_senha)==False:
-                                print("""Senha inválida. Requisitos:
-- Mínimo 8 caracteres              
-- Pelo menos 1 letra maiúscula              
-- Pelo menos 1 letra minúscula              
-- Pelo menos 1 número     
-- Pelo menos 1 caractere especial (@$!%*?&)""")
-                            else:
-                                break
-                        
-                    user_email=input("digite seu email:")
-                    user_cpf=input("digite seu cpf (ex:000.000.000-00):")
-                    cliente.cadastro(user_name,user_senha,user_email,user_cpf)
-                    continuar = input("Deseja cadastrar outro usuário? (s/n): ")
-                    if continuar.lower() != 's':
-                        break
-            case 2:
-                user_name=input("digite seu nome:")
-                while True:
-                        user_senha=input("digite sua senha:")
-                        cliente.validar_senha(user_senha)
-                        if cliente.validar_senha(user_senha)==False:
-                                print("""Senha inválida. Requisitos:
-- Mínimo 8 caracteres              
-- Pelo menos 1 letra maiúscula              
-- Pelo menos 1 letra minúscula              
-- Pelo menos 1 número     
-- Pelo menos 1 caractere especial (@$!%*?&)""")
-                        else:
-                            break
-                if cliente.login(user_name,user_senha):
-                    break  
-
-
-
-
-
-        if opcao==3:
+    
+        if opcao == '1':
+            cliente.cadastro ( )
+        elif opcao == '2':
+            cliente.buscar_user( )
+        elif opcao == '3':
+            # atualizar ( )
+            cliente
+        elif opcao == '4':
+            # excluir ( )
+            cliente
+        elif opcao == '5':
+            print ('Encerrando  a sessão. . .'.upper())
             break
+        else:
+            print ("Opção inválida, por favor tente novamente".upper())
+        print ('Sessão encerrada.'.upper())
 
     except:
         print("formato invalido")
+        
+        # match opcao:
+            
+        #     case 2:
+                # user_name=input("digite seu nome:")
+                # user_cpf=input("digite seu cpf (ex:000.000.000-00):")
+                # for cliente.usuario in cliente.usarios:
+                #     if cliente.usuario["cpf"]==user_cpf:
+                #         print(cliente.buscar_user(user_name,user_cpf))
+
+        # if opcao==3:
+        #     break
+    
--- utils/cliente.py
+++ utils/cliente.py
@@ -11,26 +11,71 @@ def validar_senha(senha):
     else:
         return False
         
-def login(nome,senha):
+def buscar_user(nome,cpf):
     for usuario in usuarios:
-        if usuario["nome"] == nome and usuario["senha"]==senha:
-            True
-            break
+        if usuario["nome"] == nome and usuario["cpf"]==cpf:
+            for i, u in enumerate(usuarios, start=1):
+                return print(f"{i}. Nome: {u['nome']}, Idade: {u['idade']}, Email: {u['email']}")
+            
 
+def validar_cpf(cpf):
+    cpf_valido=False
+    if len(cpf)==14:
+        usuario["cpf"]= cpf.replace('.','').replace('-','')
+        cpf_valido=True        
+        return cpf_valido
+        
     
     
     
 def senha_aleatoria():
     random_senha = ''.join(random.choices(string.ascii_letters+string.digits+string.punctuation, k=8))
     usuario["senha"]=random_senha
+    return random_senha
 
 
-def cadastro(nome,senha,email,cpf):
+def cadastro( ):
+    while True:
+        user_name=input("digite seu nome:")
+        senha_escolha=input("deseja ter uma senha aleatoria(s ou n)?").lower()
+        if senha_escolha == 's':
+            user_senha=senha_aleatoria
+            print("1- ver senha")
+            print("2- nao ver senha")
+            try:
+                senha_ver=int(input("escolha uma das opções:"))
+            except:
+                print("formato invalido")
+            if senha_ver == 1:
+                print(senha_aleatoria)
+        else:
+            while True:
+                
+                user_senha=input("digite sua senha:")
+                validar_senha(user_senha)
+                if validar_senha(user_senha)==False:
+                    print("""Senha inválida. Requisitos:
+    - Mínimo 8 caracteres                   
+    - Pelo menos 1 letra maiúscula              
+    - Pelo menos 1 letra minúscula              
+    - Pelo menos 1 número     
+    - Pelo menos 1 caractere especial (@$!%*?&)""")
+                else:
+                    break
+                
+            user_email=input("digite seu email:")
+            while True:
+                user_cpf=input("digite seu cpf (ex:000.000.000-00):")
+                if validar_cpf(user_cpf)==True:
+                    cadastro(user_name,user_senha,user_email,user_cpf)
+                    break
+            continuar = input("Deseja cadastrar outro usuário? (s/n): ")
+            if continuar.lower() != 's':
+                break 
     usuario = {
-        "nome": nome,
-        "senha": senha,
-        "email": email,
-        "cpf": cpf
+        "nome": user_name,
+        "senha": user_senha,
+        "email": user_email
     }
     usuarios.append(usuario)
         
